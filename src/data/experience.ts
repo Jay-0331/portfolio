8f0c06acdb31afdc0699195f1aef7c46887c1d50
jay vekariya <vekariya.ja@northeastern.edu>
2024-11-13 21:28:08 -0500

    chore: exp update

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -4,7 +4,7 @@ export const experienceData: ExperienceType[] = [
 		company: 'Infosys',
 		date: 'Jan 2021 - Jul 2023',
 		description:
-			'Led development of a React.js/Typescript front-end for sales team and optimized Java/Springboot backend, including streamlining Jenkins pipeline and implementing automated scripts, resulting in improved system efficiency and sales performance.',
+			'Led development of a React.js/Typescript front-end for sales team and optimized Java/Springboot backend, including streamlining Jenkins pipeline and implementing automated scripts, resulting in improved system efficiency and performance.',
 	},
 ]
 
